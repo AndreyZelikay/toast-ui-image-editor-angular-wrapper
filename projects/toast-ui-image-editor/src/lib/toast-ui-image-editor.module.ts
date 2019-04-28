5ed87827d91ee4bb67c1d467c6836e3775a6db11
Andrey <andrey_zelikay@mail.ru>
2019-04-28 16:58:35 +0300

    Changing code according to my personal Angular case

--- projects/toast-ui-image-editor/src/lib/toast-ui-image-editor.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { ToastUiImageEditorComponent } from "./toast-ui-image-editor.component";
-
-@NgModule({
-  imports: [],
-  declarations: [ToastUiImageEditorComponent],
-  exports: [ToastUiImageEditorComponent]
-})
-export class ToastUiImageEditorModule {}
--- projects/toast-ui-image-editor/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of toast-ui-image-editor
- */
-
-export * from "./lib/toast-ui-image-editor.component";
-export * from "./lib/toast-ui-image-editor.module";
-export * from "./lib/toast-ui-image-editor.models";
